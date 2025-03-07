4f3a66c91fa8f43c79a7e7585e9cb1440758b6db
Veniamin <ven_ic@mail.ru>
2025-03-07 11:58:52 +0700

    VUE Proba

--- scripte.js
+++ scripte.js
@@ -1,5 +1,12 @@
-document.body.innerHTML = '<h1> Hello from Javascript</h1>'
+// document.body.innerHTML = '<h1> Hello from Javascript</h1>'
 
 console.log('Test 2')
 console.log('Test 45')
 
+console.log('проверка')
+import { createApp } from 'vue'
+import App from './index.vue'
+import router from './router'
+import './theme.css'
+
+createApp(App).mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    ПАривет мне
+  </h1>
+</template>
+
+<script>
+ export default {
+   name:"Index"
+ }
+</script>
